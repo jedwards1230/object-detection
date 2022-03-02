8cfa4d573c0de2f57bae06914afe7fd4b373d994
jedwards1230 <justinedwards1230@gmail.com>
2022-03-02 17:33:57 -0500

    organize classes

--- cv2-boilerplate.py
+++ cv2-boilerplate.py
@@ -1,10 +1,28 @@
 import cv2
 from imutils.video import FPS
+from Detectors import (
+    HandDetector,
+    PoseDetector)
 
 video_stream = "http://192.168.1.143:56000/mjpeg"
 
+modules = [
+    'hand',
+    'pose'
+]
+
+to_test = modules[1]
+
 if __name__=='__main__':
     vcap = cv2.VideoCapture(video_stream)
+    if not vcap.isOpened():
+        print("Cannot open camera")
+        exit()
+        
+    if to_test == modules[0]:
+        hd = HandDetector()
+    elif to_test == modules[1]:
+        pd = PoseDetector()
     
     while True:
         success, img = vcap.read()
@@ -12,6 +30,13 @@ if __name__=='__main__':
             img = cv2.flip(img, 1)
             fps = FPS().start()
             H, W, C = img.shape
+            
+            if to_test == modules[0]:
+                hd.find_hands(img, draw=True)
+                landmark_list = hd.find_position(img, print_coords=False)
+            elif to_test == modules[1]:
+                pd.find_pose(img, draw=True)
+                landmark_list = pd.find_position(img, draw=False, print_coords=False)
             
             fps.update()
             fps.stop()
